be25618ca7089f5fb944821635070fd22e0bcb12
TsaqifHasbi <tsaqifhasbi1717@gmail.com>
2024-11-29 02:56:34 +0700

    front end ready

--- TheWildHunt3_FrontEnd/Profile.php
+++ TheWildHunt3_FrontEnd/Profile.php
@@ -1,13 +1,77 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- Boxicons Cdn Link -->
     <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="DesignSideNav1.css" />
+    <link rel="stylesheet" href="DesignSideNav.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>My Web</title>
+    <title>My Profile</title>
+    <style>
+      .data img {
+        width: 200px;
+        max-width: 100%;
+        height: 200px;
+        border-radius: 10px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      h2 {
+        margin-top: 15px;
+      }
+      p {
+        margin-top: 15px;
+        text-align: center;
+        font-size: 16px;
+      }
+      .data {
+        padding-top: 140px;
+      }
+      .sm {
+        display: flex;
+        justify-content: center;
+        margin: 20px 0px;
+      }
+      .sm a {
+        color: black;
+        width: 56px;
+        font-size: 40px;
+        margin: 0 15px;
+      }
+      .sm .ig a {
+        color: black;
+        transition: 0.3s linear;
+      }
+      .sm .ig a:hover {
+        background: linear-gradient(to top right, yellow, orange, red, purple, blue);
+        -webkit-background-clip: text;
+        background-clip: text;
+        color: transparent;
+        font-size: 50px;
+      }
+      .sm .tw a {
+        color: black;
+        transition: 0.3s linear;
+      }
+      .sm .tw a:hover {
+        color: rgb(62, 154, 190);
+        -webkit-background-clip: text;
+        background-clip: text;
+        font-size: 50px;
+      }
+      .sm .sptfy a {
+        color: black;
+        transition: 0.3s linear;
+      }
+      .sm .sptfy a:hover {
+        color: green;
+        -webkit-background-clip: text;
+        background-clip: text;
+        font-size: 50px;
+      }
+    </style>
   </head>
   <body>
     <div class="sidebar">
@@ -34,7 +98,7 @@
           <span class="tooltip">Dashboard</span>
         </li>
         <li>
-          <a href="Profile.html">
+          <a href="#">
             <i class="bx bx-user"></i>
             <span class="links_name">User</span>
           </a>
@@ -87,7 +151,7 @@
         <div class="profile">
           <div class="profile_details">
             <a href="Profile.html">
-              <img src="PP.jpeg" alt="" />
+              <img src="Gambar/PP.jpeg" alt="" />
               <div class="name_job">
                 <div class="name">Tsaqif Hasbi</div>
                 <div class="job">Web Designer</div>
@@ -99,9 +163,30 @@
       </div>
     </div>
     <div class="home_content">
-      <div class="text">Home Content</div>
+      <div class="data">
+        <img src="Gambar/PP.jpeg" alt="Foto_Profile" title="Foto Profile Saya" />
+        <h2 style="text-align: center">Tsaqif Hasbi Aghna Syarief 20209241</h2>
+        <p>XIT11</p>
+        <p>
+          Nama saya Tsaqif Hasbi Aghna Syarief, saya adalah seorang pelajar<br />
+          saya bersekolah di SMKS Telkom Sandhy Putra Jakarta, saya kelas 11<br />
+          dikelas ini saya belajar banyak tentang bahasa pemrograman dan mendesain web<br />
+          saya berumur 16 tahun dan saya tinggal di kecamatan kalideres, Jakarta Barat
+        </p>
+        <p>Follow My Social Media & My Spotify</p>
+        <div class="sm">
+          <div class="ig">
+            <a href="https://www.instagram.com/tsaqif_hasbi/"><i class="bx bxl-instagram"></i></a>
+          </div>
+          <div class="tw">
+            <a href="https://twitter.com/TsaqifHasbi17?t=jot9H78I46MgPBj14Xk12w&s=08"><i class="bx bxl-twitter"></i></a>
+          </div>
+          <div class="sptfy">
+            <a href="https://open.spotify.com/user/21c2iygultzcmsl6lisvlu7tq?si=fyURqvnZTxWf1PttYhtcgQ&utm_source=copy-link"><i class="bx bxl-spotify"></i></a>
+          </div>
+        </div>
+      </div>
     </div>
-
     <script>
       let button = document.querySelector('#button');
       let sidebar = document.querySelector('.sidebar');
