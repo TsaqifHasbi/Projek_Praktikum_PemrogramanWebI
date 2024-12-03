3b0572a91a600542afa5fba300a5823b46a13dfb
TsaqifHasbi <tsaqifhasbi1717@gmail.com>
2024-12-04 04:02:38 +0700

    terakhir

--- TheWildHunt3_FrontEnd/Vizima.php
+++ TheWildHunt3_FrontEnd/Vizima.php
@@ -9,7 +9,7 @@
 <body>
     <header>
         <div class="header-content">
-            <img src="../assets/Chapter 1/LOGO.svg" alt="The Witcher Logo" class="logo">
+            <img src="../assets/Prologue/LOGO.svg" alt="The Witcher Logo" class="logo">
             <button class="profile">
                 <img class="vizima" src="../assets/Choose Region/Vizima.svg" alt="">
                 <div class="welcome">
@@ -33,26 +33,49 @@
         <h1 class="judul">Prologue</h1> 
         <h3 class="subjudul">White Orchard & Vizima</h3>
         <div class="content">
+            <img src="../assets/Prologue/pic 0.svg" alt="Prologue Scene">
             <div class="text">
-                <p>The opening cutscene shows Yennefer fleeing a massive battle in White Orchard, leaving behind traces that Geralt and Vesemir follow days later...</p>
+                <p class="para1">The opening cutscene shows Yennefer fleeing a massive battle in White Orchard, leaving behind traces that Geralt and Vesemir follow days later. The story then transitions to Kaer Morhen, the Witcher stronghold, where Geralt awakens in a guest room.</p>
+                <p class="para2">After brief tutorials on movement and combat, the Wild Hunt suddenly appears, revealing that Geralt is dreaming of past events. Upon waking, he discusses the dream with Vesemir, and they continue tracking Yennefer as depicted in the opening scene.</p>
             </div>
-            <img src="prologue-scene.png" alt="Prologue Scene">
         </div>
     </section>
 
-    <section class="events">
-        <div class="event">
-            <h3>The Griffin's Bargain</h3>
-            <p>Geralt and Vesemir arrive in White Orchard...</p>
+    <section class="events-section">
+        <img class="dots3" src="../assets/Prologue/3 dot.svg">
+        <div class="events">
+            <div class="event">
+                <h3>The Griffin's Bargain</h3>
+                <p>Geralt and Vesemir arrive in White Orchard, where they learn Yennefer spoke with Nilfgaardian Captain Peter Saar Gwynleve. The captain promises to reveal Yennefer's whereabouts if Geralt eliminates a griffin terrorizing the area. After setting a trap and defeating the beast with Vesemir, they learn Yennefer is headed to Vizima. Before leaving, Yennefer unexpectedly arrives and insists Geralt follow her to meet Emperor Emhyr var Emreis. Vesemir decides to return to Kaer Morhen.</p>
+            </div>
+            <div class="event">
+                <h3>A Wild Hunt Encounter</h3>
+                <p>On the way to Vizima, Geralt and Yennefer are attacked by hounds of the Wild Hunt but manage to escape unharmed. They reach Vizima safely, where Emperor Emhyr requests Geralt's help in finding his daughter, Ciri, pursued by the Wild Hunt. Geralt agrees to assist.</p>
+            </div>
+            <div class="event">
+                <h3>Paths to Ciri</h3>
+                <p>Geralt plans to begin his search for Ciri in Velen and team up with Triss Merigold. Yennefer, meanwhile, heads to Skellige to investigate a magical disturbance potentially tied to Ciri.</p>
+            </div>
         </div>
-        <div class="event">
-            <h3>A Wild Hunt Encounter</h3>
-            <p>On the way to Vizima, Geralt and Yennefer...</p>
+    </section>
+
+    <section class="scene">
+        <img class="dots3" src="../assets/Prologue/line.svg">
+        <div class="gallery">
+            <div class="card">
+                <img src="../assets/Prologue/pic 1.svg" alt="Geralt of Rivia killed the griffin" />
+                <p>Geralt of Rivia killed the griffin</p>
+            </div>
+            <div class="card">
+                <img src="../assets/Prologue/pic 2.svg" alt="Yennefer manage to escape from the Wild Hunt" />
+                <p>Yennefer manage to escape from the Wild Hunt</p>
+            </div>
+            <div class="card">
+                <img src="../assets/Prologue/pic 3.svg" alt="First look of Ciri's teenage version" />
+                <p>First look of Ciri's teenage version</p>
+            </div>
         </div>
-        <div class="event">
-            <h3>Paths to Ciri</h3>
-            <p>Geralt plans to begin his search for Ciri...</p>
-        </div>
+        <img class="dots3re" src="../assets/Prologue/line.svg">
     </section>
 
     <section class="choices">
