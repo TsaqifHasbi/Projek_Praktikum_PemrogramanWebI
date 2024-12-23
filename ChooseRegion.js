3b2ae6aa50c7630d9e3b183f6738f929f4964070
TsaqifHasbi <tsaqifhasbi1717@gmail.com>
2024-12-23 20:10:07 +0700

    testing

--- ChooseRegion.js
+++ ChooseRegion.js
@@ -13,7 +13,7 @@ document.querySelectorAll('.region-card').forEach(card => {
         card.addEventListener('click', () => {
             const region = card.getAttribute('data-region');
             selectedRegionInput.value = region;
-            regionForm.submit(); // Kirim form
+            regionForm.submit();
         });
     });
 });
